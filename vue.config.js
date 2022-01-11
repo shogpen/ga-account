f2edd7bfbe999b7b8297a0ff0327f74ac92a527b
shogpen <shogpenguin@gamil.com>
2022-01-12 00:47:09 +0900

    家計簿完成

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  publicPath: isProd ? '/account-book' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
